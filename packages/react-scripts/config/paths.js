f7f5c64692a41f5cf3b55823637c819492d33609
Taylor Bell <taylorbell@gmail.com>
2016-12-13 12:27:51 -0700

    add assets directory to paths

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -48,6 +48,7 @@ module.exports = {
   appIndexJs: resolveApp('src/index.js'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
+  appStyles: resolveApp('src/assets/stylesheets'),
   yarnLockFile: resolveApp('yarn.lock'),
   testsSetup: resolveApp('src/setupTests.js'),
   appNodeModules: resolveApp('node_modules'),
